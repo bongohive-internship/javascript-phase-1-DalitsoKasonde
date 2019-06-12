5833de53f1382aea0e7388d799c2c295928f8bfc
DalitsoKasonde <dkasonde11@gmail.com>
2019-06-13 00:04:17 +0200

    Add javascript code for phase 1

--- variables.js
+++ variables.js
@@ -5,4 +5,14 @@
 let birthYear = 1995;
 let futureYear = 2050;
 
-console.log('Normal age is ' + futureYear - birthYear );
+console.log('Normal age is ' + (futureYear - birthYear) );
+console.log('Other possible is ' + ( birthYear - futureYear) );
+
+// Question 2
+let age = 23;
+let maxAge = 70;
+let amount = 10;
+
+console.log("You will need "+ ((maxAge - age) * amount) + " to last you until the ripe old age of " + maxAge);
+
+
